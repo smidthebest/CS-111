7ebf94739c6eb9d1df570180944428bea62775f8
Siddhartha Mishra <siddhartham04@gmail.com>
2023-10-03 10:18:49 -0700

    changes

--- proc_count.c
+++ proc_count.c
@@ -7,7 +7,12 @@
 static struct proc_dir_entry *entry;
 
 static int proc_count(struct seq_file *m, void *v){
-	// TODO: it's all yours
+	 int i; 
+    struct task_struct *p; 
+    for_each_process(p){
+        i++; 
+    }
+    seq_printf(m, i); 
 	return 0;
 }
 
